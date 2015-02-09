8a4042c7a442bb0ed3bfc635309a5ecbda78ad2b
m2q1n9 <m2q1n9@users.noreply.github.com>
2015-02-09 15:45:02 +0800

    update

--- brew.sh
+++ brew.sh
@@ -13,7 +13,64 @@ brew update
 brew upgrade
 
 # Install command-line tools using Homebrew.
-# brew install coreutils
+brew install ack
+brew install android-ndk
+brew install android-sdk
+brew install ant
+brew install aspell
+brew install autoconf
+brew install automake
+brew install bash
+brew install bash-completion
+brew install binutils
+brew install bison
+brew install boot2docker
+brew install brew-cask
+brew install bsdiff
+brew install bsdmake
+brew install capstone
+brew install cheat
+brew install chisel
+brew install cmake
+brew install coreutils
+brew install ctags
+brew install dhex
+brew install docker
+brew install emacs
+brew install figlet
+brew install findutils
+brew install fish
+brew install flex
+brew install gawk
+brew install gcc
+brew install git
+brew install global
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-typist
+brew install hub
+brew install imagemagick
+brew install jenkins
+brew install lua
+brew install luajit
+brew install luarocks
+brew install mobile-shell
+brew install mongodb
+brew install mysql
+brew install nginx
+brew install node
+brew install python
+brew install redis
+brew install sqlite
+brew install ssh-copy-id
+brew install subversion
+brew install the_silver_searcher
+brew install vbindiff
+brew install w3m
+brew install wget
+brew install xctool
+brew install zsh
+brew install zsh-completions
 
 # Remove outdated versions from the cellar.
 brew cleanup
