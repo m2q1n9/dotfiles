01ef04555005c3ce5a3c6dc9e5acb2e42e53d84d
m2q1n9 <m2q1n9@users.noreply.github.com>
2015-02-11 18:25:02 +0800

    update

---
+++ pip.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+pip install ipython
+pip install pip-tools
+pip install pyOpenSSL
+pip install virtualenv
+pip install virtualenvwrapper
