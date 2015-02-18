2874eedd321898c27cfeeeeed9520a598b1e6003
m2q1n9 <m2q1n9@users.noreply.github.com>
2015-02-18 22:51:49 +0800

    update

--- pip.sh
+++ pip.sh
@@ -6,6 +6,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 pip install cheat
 pip install ipython
 pip install pip-tools
+pip install Pygments
 pip install pyOpenSSL
 pip install virtualenv
 pip install virtualenvwrapper
